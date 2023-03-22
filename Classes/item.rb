f74311e0bdd0f88a00854233f511243a5fb250ac
Dell Pc <mudasir9757@gmail.com>
2023-03-22 13:06:47 +0500

    Update dir Struct and item class

--- Classes/item.rb
+++ Classes/item.rb
@@ -3,29 +3,36 @@ require 'date'
 class Item
   attr_reader :archived
   attr_accessor :genre, :author, :source, :label, :publish_date
-
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(publish_date)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = DateTime.parse(publish_date)
     @archived = false
   end
 
-  def move_to_archive
-    @archived = true if can_be_archive?
+  def add_genre(genre)
+    @genre = genre
   end
 
-  # private
+  def add_author(author)
+    @author = author
+  end
 
-  def can_be_archive?
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def can_be_archived?
     current_year = DateTime.now.year
     publish_year = @publish_date.year
     @archived = publish_year <= current_year - 10
     @archived
   end
 end
-
-# item1 = Item.new('action','me','sd','label', '2009-06-01') item1.can_be_archive?
