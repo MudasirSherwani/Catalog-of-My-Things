50bbcec3aa8d24d2781e73071afb02f99dcb921b
george827 <kinyanjuigeorge827@gmail.com>
2023-03-23 16:50:31 +0300

    fix conflict

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
-# require_relative './game_logic'
-# require_relative './path_finder'
-# require_relative './load_game_data'
+require_relative './Classes/Game/game_logic'
+require_relative './Classes/Game/path_finder'
+require_relative './Classes/Game/load_game_data'
+require_relative './Classes/Game/get_user_data'
+
+require_relative 'Classes/Music/music'
 require_relative './Classes/Book/booklablemodule'
 
 class Main
@@ -11,15 +14,18 @@ class Main
   end
 
   def director(user_answer)
+    music = Music.new
     case user_answer
     when 1
       list_all_books
     when 2
       # '2 List all music albums'
+      music.list_music
     when 3
       all_games(@all_games)
     when 4
       # '4 List all genres'
+      music.list_genre
     when 5
       list_all_labels
     when 6
@@ -30,20 +36,20 @@ class Main
   end
 
   def director_add_items(user_answer)
+    music = Music.new
     case user_answer
     when 7
       add_book
     when 8
       # '8 Add a music album'
+      music.add_musicalbum
     when 9
       puts 'Follow the prompts to add a game'
       add_game(@all_games, @all_authors)
     when 10
       puts 'Thank you for using this app'
-      # path_finder(@all_games, @all_authors)
-      # puts @all_authors
-      puts ''
-      # puts @all_games
+      path_finder(@all_games, @all_authors)
+
       exit
     else
       puts 'Invalid Input Try Again !'
@@ -53,7 +59,7 @@ class Main
   def show_list
     puts ''
     puts 'Welcome to Catalog of My Things App!'
-    # load_game_data(@all_games, @all_authors)
+    load_game_data(@all_games, @all_authors)
     loop do
       puts '1 List all books'
       puts '2 List all music albums'
