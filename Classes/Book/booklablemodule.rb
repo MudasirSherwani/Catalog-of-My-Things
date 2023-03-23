442d6858f0c12f7079200aa7b73ff04825affbf9
george827 <kinyanjuigeorge827@gmail.com>
2023-03-23 14:29:35 +0300

    add book and label add method

---
+++ Classes/Book/booklablemodule.rb
@@ -0,0 +1,93 @@
+# require_relative '../item'
+require 'json'
+require_relative './book'
+require_relative './label'
+
+module BookLabel
+  def fetch_books
+    # unless File.exist?('../../storage/books.json')
+    File.open('./storage/books.json', 'w') unless File.exist?('./storage/books.json')
+    book_data = File.read('./storage/books.json')
+
+    if book_data.empty?
+      []
+    else
+      File.open('./storage/books.json', 'r') { |line| JSON.parse(line.read) }
+    end
+  end
+
+  def fetch_labels
+    File.open('./storage/labels.json', 'w') unless File.exist?('./storage/labels.json')
+    lable_data = File.read('./storage/labels.json')
+
+    if lable_data.empty?
+      []
+    else
+      File.open('./storage/labels.json', 'r') { |line| JSON.parse(line.read) }
+    end
+  end
+
+  # add book
+  def add_book
+    print 'Publisher: '
+    publisher = gets.chomp
+    print 'Cover state[good or bad]: '
+    cover_state = gets.chomp
+    print 'Publication date (yyyy-mm-dd): '
+    publish_date = gets.chomp
+    book = Book.new(publisher, cover_state, publish_date)
+
+
+    print 'Title of the lable: '
+    title = gets.chomp.capitalize
+    print 'Color: '
+    color = gets.chomp
+
+    label = Label.new(title, color)
+    # label.add_item(book.to_json)
+    book_item = book.to_json
+    label.add_item(book_item)
+
+
+    # add_label(book)
+    store_book(book)
+    store_label(label)
+
+    puts 'label added successfully'
+    puts 'Book created successfully'
+  end
+
+  # storage
+  def store_book(book)
+    books = fetch_books
+    books << book.to_json
+    File.write('./storage/books.json', JSON.generate(books), mode: 'w')
+  end
+
+  def store_label(label)
+    labels = fetch_labels
+
+    labels << label.to_json
+    File.write('./storage/labels.json', JSON.generate(labels), mode: 'w')
+  end
+
+  # listings
+
+  def list_all_books
+    books = fetch_books
+    return puts 'There are no books stored!' if books.empty?
+
+    books.each do |book|
+      puts "Publisher: #{book['publisher']}, Cover_state: #{book['cover_state']}, Publish_date: #{book['publish_date']}"
+    end
+  end
+
+  def list_all_labels
+    labels = fetch_labels
+    return puts 'There are no labels stored!' if labels.empty?
+
+    labels.each do |label|
+      puts "Title: #{label['title']}, Color: #{label['color']}"
+    end
+  end
+end
